2bed8dcf95445f04c90efdb240533db96423a700
git branch -M main <git remote add origin https://github.com/nthm1806/tiktok-ui-H-a1862.git>
2025-02-03 01:15:59 +0700

    2/2/2025

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,173 +1,40 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Footer.css";
-// import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-// import { faMapMarkerAlt, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
     <div className="footer_wrapper">
       <div className="footer_container_first"></div>
       <div className="footer_container_second">
-        Tìm địa điểm trên đường đi? Tải app FoodG5!
+         Group5Food lan tỏa vẻ đẹp truyền thống ẩm thực Việt!
       </div>
-      <div
-        style={{
-          maxHeight: "55vh",
-          overflow: "hidden",
-          width: "86%",
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <div className="footer_container_third">
-          <div className="footer_block_third_stats">
-            <div
-              style={{
-                width: "12vw",
-                background: "white",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginBottom: "5vh",
-              }}
-            >
-              Thống Kê
-            </div>
-            <div className="footer_container_third_stats">
-              <div className="footer_container_third_stats_number">
-                334,384 Địa điểm
-              </div>
-              <div className="footer_container_third_stats_content">
-                toàn quốc
-              </div>
-            </div>
-            <div className="footer_container_third_stats">
-              <div className="footer_container_third_stats_number">
-                38,630,265 người sử dụng
-              </div>
-              <div className="footer_container_third_stats_content">
-                trong & ngoài nước
-              </div>
-            </div>
-            <div className="footer_container_third_stats">
-              <div className="footer_container_third_stats_number">
-                1,481,841 bình luận
-              </div>
-              <div className="footer_container_third_stats_content">
-                đã chia sẻ
-              </div>
-            </div>
-            <div className="footer_container_third_stats">
-              <div className="footer_container_third_stats_number">
-                608,066 check-in
-              </div>
-              <div className="footer_container_third_stats_content">
-                đã thực hiện
-              </div>
-            </div>
-            <div className="footer_container_third_stats">
-              <div className="footer_container_third_stats_number">
-                10,232,333 hình ảnh
-              </div>
-              <div className="footer_container_third_stats_content">
-                được tải lên
-              </div>
-            </div>
-            <div className="footer_container_third_stats">
-              <div className="footer_container_third_stats_number">
-                24,623,376 Bộ sưu tập
-              </div>
-              <div className="footer_container_third_stats_content">
-                bộ sưu tập được tạo
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="footer_container_third_img_app">
-          <div className="footer_container_third_img">
-            <div
-              style={{
-                width: "12vw",
-                background: "white",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginBottom: "5vh",
-              }}
-            >
-              iOS App
-            </div>
-            <img
-              className="footer-img_first_and_third"
-              src="img_first_app.png"
-              alt=""
-            />
-          </div>
-          <div className="footer_container_third_img">
-            <div
-              style={{
-                width: "12vw",
-                background: "white",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginBottom: "5vh",
-              }}
-            >
-              Android App
-            </div>
-            <img
-              className="footer-img_first_and_third"
-              src="img_second_app.png"
-              alt=""
-            />
-          </div>
-          <div className="footer_container_third_img">
-            <div
-              style={{
-                width: "12vw",
-                background: "white",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginBottom: "5vh",
-              }}
-            >
-              Windows App
-            </div>
-            <img src="img_third_app.png" alt="" />
-          </div>
-        </div>
+      <div className="footer_container_third">
+        <img src="Banhcuon.png" alt=""/>
+        <img src="Pho.png" alt=""/>        
+        <img src="Banhcuon.png" alt=""/>
+        <img src="Bundau.png" alt=""/>
+        <img src="Chanem.png" alt=""/>
+        <img src="Comtam.png" alt=""/>
+        <img src="Gatan.png" alt=""/>
+        <img src="Phocuon.png" alt=""/>
+        <img src="VitQuay.png" alt=""/>
+        <img src="XoiCom.png" alt=""/>
+        <img src="Sup.png" alt=""/>
+        <img src="LongLon.png" alt=""/>
+        <img src="LonQuay.png" alt=""/>
+        <img src="NemChua.png" alt=""/>
       </div>
       <div className="footer_block_middle">
-        <div className="footer_block_middle_box">
-          <div className="footer_block_middle_box_above">Khám phá</div>
-          <li>Ứng dụng Mobile</li>
-          <li>Tạo bộ sưu tập</li>
-          <li>Bảo mật thông tin</li>
-          <li>Quy định</li>
+        <img src="logo.png" alt=""/>
+      </div>
+      <div className="footer_block_end">
+        <div className="footer_block_end_box_first">
+          Công Ty Cổ Phần Group5Food, thôn 3, Thạch Hòa, Thạch Thất, Hà Nội
         </div>
-        <div className="footer_block_middle_box">
-          <div className="footer_block_middle_box_above">Công ty</div>
-          <li>Giới thiệu</li>
-          <li>Trợ giúp</li>
-          <li>Việc làm</li>
-          <li>Quy chế</li>
-          <li>Thỏa thuận sử dụng dịch vụ</li>
-          <li>Liên hệ</li>
+        <div className="footer_block_end_box_first">
+          Email: support@group5food.vn
         </div>
-        <div className="footer_block_middle_box">
-          <div className="footer_block_middle_box_above">Tham gia trên</div>
-          <li>Facebook</li>
-          <li>Instagram</li>
-          <li>Youtube</li>
-          <li>Google</li>
-          <li>Group5Food.vn - Ship tận nơi</li>
-        </div>
-        <div className="footer_block_middle_box">
-          <div className="footer_block_middle_box_above">Giấy phép</div>
-          <li>MXH 363/GP-BTTTT</li>
-          <img className="bocongthuong_img" src="bocongthuong.png" alt="" />
+        <div className="footer_block_end_box_first">
+          Hotline: 0966.88.1862 (Mr.Hòa)
         </div>
       </div>
     </div>
