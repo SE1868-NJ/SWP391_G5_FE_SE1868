46dc44ac3bf1bba57a211ad69b94e8ca13c32674
viet30072001 <vietddhe151061@fpt.edu.vn>
2025-02-02 20:52:07 +0700

    add menu searcg

--- src/router/products/SearchProducts.jsx
+++ src/router/products/SearchProducts.jsx
@@ -1,10 +1,12 @@
 import { ListProduct } from "../../components/products/ListProduct"
 import { LayoutCommon } from "../../layout/layout-common/LayoutCommon";
+import { MenuSearch } from "./MenuSearch";
 import './styles.css'
 function SearchProduct() {
     return (
         <LayoutCommon>
             <div className="container-list">
+                <MenuSearch/>
                 <div className="list-product">
                     <ListProduct />
                 </div>
