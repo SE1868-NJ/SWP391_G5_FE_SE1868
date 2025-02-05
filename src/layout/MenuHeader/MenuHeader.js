1a9ce99af69b769265f760a076a198d3c4663c69
git branch -M main <git remote add origin https://github.com/nthm1806/tiktok-ui-H-a1862.git>
2025-02-06 00:44:11 +0700

    5/2

--- src/layout/MenuHeader/MenuHeader.js
+++ src/layout/MenuHeader/MenuHeader.js
@@ -5,11 +5,31 @@ function MenuHeader() {
   const [hover, setHover] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [productList, setProductList] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
+  const [activeCategory, setActiveCategory] = useState(-1);
+  const [activeType, setActiveType] = useState(0);
+  const [option, setOption] = useState("Tất Cả");
+  const [type, setType] = useState("New");
 
-  async function allNew(option, type) {
+  async function allNew() {
     try {
-      const response = await axios.post("http://localhost:3001/api/Products/All/New",{ option, type });
-      setProductList(response.data);
+      const response = await axios.get(
+        "http://localhost:3001/api/Products/All/New?",
+        { params: { option, type } }
+      );
+      setProductList(response.data[0]);
+      console.log(response.data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function allCategory() {
+    try {
+      const response = await axios.get(
+        "http://localhost:3001/api/Products/Category"
+      );
+      return setCategoryList(response.data[0]);
     } catch (error) {
       console.log(error);
     }
@@ -21,6 +41,7 @@ function MenuHeader() {
         className="fhs_option_header_span"
         onClick={() => {
           setIsMenuOpen(!isMenuOpen);
+          allCategory();
         }}
         onMouseLeave={() => {
           if (!isMenuOpen) setHover(false);
@@ -61,14 +82,80 @@ function MenuHeader() {
           }}
         >
           <div className="option">
-            <div className="items_option">Tất Cả</div>
-            {productList.length > 0 ? productList.map((item) => (
-    <div key={item.ProductID} className="items_option">{item.Category}</div>
-)) : <p>Không có dữ liệu</p>}
-
+            <div
+              className={`items_option ${
+                activeCategory === -1 ? "active" : ""
+              }`}
+              onClick={() => {
+                setActiveCategory(-1);
+                setOption("Tất Cả");
+              }}
+            >
+              Tất Cả
+            </div>
+            {Array.isArray(categoryList) && categoryList.length > 0 ? (
+              categoryList.map((item, index) => (
+                <div
+                  key={index}
+                  className={`items_option ${
+                    activeCategory === index ? "active" : ""
+                  }`}
+                  onClick={() => {
+                    setActiveCategory(index);
+                    setOption(item.Category);
+                  }}
+                >
+                  {item.Category}
+                </div>
+              ))
+            ) : (
+              <p>Không có dữ liệu</p>
+            )}
           </div>
-          <div>
-            <button onClick={() => allNew("Tất Cả", "New")}>hi</button>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <div className="type">
+              <div
+                className={`items_type ${activeType === 0 ? "active" : ""}`}
+                onClick={() => {
+                  setActiveType(0);
+                  setType("New");
+                }}
+              >
+                Mới Nhất
+              </div>
+              <div
+                className={`items_type ${activeType === 1 ? "active" : ""}`}
+                onClick={() => {
+                  setActiveType(1);
+                  setType("Rẻ Nhất");
+                }}
+              >
+                Rẻ Nhất
+              </div>
+              <div
+                className={`items_type ${activeType === 2 ? "active" : ""}`}
+                onClick={() => {
+                  setActiveType(2);
+                  setType("Bán Chạy Nhất");
+                }}
+              >
+                Bán Chạy Nhất
+              </div>
+            </div>
+            <div className="showProducts">
+              <div className="items_showProducts">
+                hi
+              </div>
+              <div className="items_showProducts">
+                hi
+              </div>
+              <div className="items_showProducts">
+                hi
+              </div>
+              <div className="items_showProducts">
+                hi
+              </div>
+            </div>
           </div>
         </div>
       )}
