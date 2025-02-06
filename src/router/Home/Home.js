b78e2a09b313838b599112ace9960dc331768aeb
git branch -M main <git remote add origin https://github.com/nthm1806/tiktok-ui-H-a1862.git>
2025-02-07 00:41:43 +0700

    7/2

--- src/router/Home/Home.js
+++ src/router/Home/Home.js
@@ -1,12 +1,14 @@
 import Header from "../../layout/Header/Header";
 import Footer from "../../layout/Footer/Footer";
 import Background from "../../layout/Background/Background";
+import Main from "../../layout/Main/Main";
 
 function Home() {
   return (
     <div>
       <Header />
       <Background>
+        <Main></Main>
         <Footer></Footer>
       </Background>
     </div>
