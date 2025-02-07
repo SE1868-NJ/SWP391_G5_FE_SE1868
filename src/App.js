78d3e93627a260fe14a0b62cf1b6f61c529a1e48
anhduc111 <154323330+anhduc111@users.noreply.github.com>
2025-02-07 08:00:48 +0700

    use context

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Header from "./layout/Header/Header";
 import Order from "./router/Order/Order.js";
 import Home from "./router/Home/Home";
 import Prepay from "./router/Prepay/Prepay.js"
+import StatusOrder from './router/StatusOrder/StatusOrrder.js'
+import { createContext } from "react";
 
 function App() {
+
   return (
     <Router>
       <Routes>
-        <Route path="/Order" element={<Home/>} />
-        <Route path="/Login" element={<Header />} />
-        <Route path="/" element={<Order />} />
+        <Route path="/Order" element={<Order/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/StatusOrder" element={<StatusOrder />} />
         <Route path="/Prepay" element={<Prepay />} />
       </Routes>
     </Router>
