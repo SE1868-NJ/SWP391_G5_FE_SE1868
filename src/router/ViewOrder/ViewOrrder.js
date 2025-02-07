d7e0b293aa75d2209600142bb2c323a15c58d790
Dungohio <dungspeed09@gmail.com>
2025-02-07 23:12:48 +0700

    Update ViewOrrder.js

--- src/router/ViewOrder/ViewOrrder.js
+++ src/router/ViewOrder/ViewOrrder.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Header from '../../layout/Header/Header'
-import styles from './StatusOrder.module.css'
-import {useCustomer} from '../../Context'
+import styles from './ViewOrder.module.css'
+import { useCustomer } from '../../Context'
 
 function StatusOrrder() {
-    const {customer} = useCustomer();
+  const { customer } = useCustomer();
   return (
     <div>
       <Header></Header>
       <div className={styles.content}>
         <div className={styles.innerContent}>
-             <div className={styles.index}>
-                    <img alt='' src={customer.Avatar}  />
-             </div>
+          <div className={styles.index}>
+            <img alt='' src={customer.Avatar} />
+          </div>
         </div>
       </div>
     </div>
