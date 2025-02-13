d436bd191d4d7acf26505883a043cc9970db2622
anhduc111 <154323330+anhduc111@users.noreply.github.com>
2025-02-14 01:07:57 +0700

    handle conflict

--- src/App.js
+++ src/App.js
@@ -19,32 +19,17 @@ import { GlobalProvider } from "./globalContext/GlobalContext";
 function App() {
 
   return (
-<<<<<<< HEAD
-    <Router>
-      <Routes>
-        <Route path="/OrderCheckOut" element={<Order/>} />
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Header />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Header />} />
-        <Route path="/search" element={<SearchProduct />} />
-        <Route path="/Order" element={<Order/>} />
-        <Route path="/" element={<Home />} />
-        <Route path="/OrderandVoucher" element={<OrderandVoucher />} />
-        <Route path="/Prepay" element={<Prepay />} />
-      </Routes>
-    </Router>
-=======
     <GlobalProvider>
       <Router>
         <Routes>
+        <Route path="/OrderandVoucher" element={<OrderandVoucher />} />
+        <Route path="/Prepay" element={<Prepay />} />
           <Route path="/OrderCheckOut" element={<Order />} />
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Header />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/search" element={<SearchProduct />} />
           <Route path="/Order" element={<Order />} />
-          <Route path="/ViewOrder" element={<ViewOrder />} />
           <Route path="/Prepay" element={<Prepay />} />
           <Route path="customers/*" element={<CustomerRoutes />} />
           <Route path="/Category" element={<Category />} />
@@ -54,7 +39,6 @@ function App() {
         </Routes>
       </Router>
     </GlobalProvider>
->>>>>>> 020c16c97ac5dbbd9b92ae6899c783fa6e1e3d6a
   );
 }
 
